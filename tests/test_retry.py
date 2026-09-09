db8314082328d2cfa3611789bc741b539400c1be
errital <mohammed.erritali@iutbayonne.univ-pau.fr>
2026-09-09 21:51:00 +0200

    ajout de retry 3

--- tests/test_retry.py
+++ tests/test_retry.py
@@ -5,6 +5,6 @@ COUNTER_FILE = Path("/tmp/retry-demo")
 def test_retry():
     if not COUNTER_FILE.exists():
         COUNTER_FILE.write_text("1")
-        assert False # "Échec volontaire pour tester le retry"
+        assert False 
 
     assert True
